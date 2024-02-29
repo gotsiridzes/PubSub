d9ef1f1ffd3123f2257d40b70fc63acd396ab0f5
Saba Gotsiridze <gotsiridze.s@outlook.com>
2024-02-29 14:49:59 +0400

    Add publish message endpoint

--- MessageBroker/Program.cs
+++ MessageBroker/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<AppDbContext>(ops =>
 var app = builder.Build();
 
 
+// Create Topic
 app.MapPost("api/topics", async (AppDbContext dbContext, Topic topic) =>
 {
 	await dbContext.Topics.AddAsync(topic);
@@ -21,11 +22,47 @@ app.MapPost("api/topics", async (AppDbContext dbContext, Topic topic) =>
 	return Results.Created($"api/topics/{topic.Id}", topic);
 });
 
+// Return topics
 app.MapGet("api/topics", async (AppDbContext dbContext) =>
 {
 	var topics = await dbContext.Topics.ToListAsync();
 
 	return Results.Ok(topics);
+});
+
+// Publish message
+app.MapPost("/api/topics/{id}/messages", async (AppDbContext dbContext, int id, Message message) =>
+{
+	var topics = await dbContext.Topics.AnyAsync(t => t.Id.Equals(id));
+
+	if (!topics)
+		return Results.NotFound($"Topic with id `{id}` not found");
+
+	var subs = await dbContext.Subscriptions.Where(s => s.TopicId.Equals(id)).ToListAsync();
+
+	if (subs.Count == 0)
+		return Results.NotFound("There are no subscriptions for this topic");
+
+	//subs.Aggregate((subs2) =>
+	//{
+	//	subs2.
+	//});
+
+	subs.ForEach(async (sub) =>
+	{
+		await dbContext.Messages.AddAsync(new Message
+		{
+			TopicMessage = message.TopicMessage,
+			ExpiresAfter = message.ExpiresAfter,
+			MessageStatus = message.MessageStatus,
+			SubscriptionId = sub.Id
+		});
+	});
+
+
+	await dbContext.SaveChangesAsync();
+
+	return Results.Ok("Message has been published");
 });
 
 app.Run();
